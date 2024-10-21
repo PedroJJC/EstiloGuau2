e0df0fb21fe69e3aa2360a15635e2403cb354ab9
JoelCetina <21090788@alumno.utmetropolitana.edu.mx>
2024-10-21 16:41:29 -0600

    QUE NO SE MUERA
    DADDD

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '12345',
+  password: '',
   database: 'bdestiloguau'
 });
 
@@ -646,7 +646,7 @@ app.get('/all-ofertas', (req, res) => {
 //Pedro PRODUCTOS
 // Obtener todos los productos
 app.get('/productos', (req, res) => {
-  const query = 'SELECT *, SUBSTRING_INDEX(producto.foto, \',\', 1) AS primera_foto FROM producto';
+  const query = 'SELECT p.*, SUBSTRING_INDEX(p.foto, \',\', 1) AS primera_foto,  o.oferta AS porcentaje_descuento FROM  producto p LEFT JOIN ofertas o ON p.idOferta = o.idOferta;';
   connection.query(query, (error, results) => {
     if (error) {
       res.status(500).json({ message: error.message });
@@ -674,7 +674,7 @@ app.get('/productosidus/:idUsuario', (req, res) => {
 
 // Obtener un producto por ID
 app.get('/productos/:id', (req, res) => {
-  const query = 'SELECT * FROM producto WHERE idProducto = ?';
+  const query = 'SELECT p.*, o.oferta AS porcentaje_descuento FROM producto p LEFT JOIN ofertas o ON p.idOferta = o.idOferta WHERE idProducto = ?';
   connection.query(query, [req.params.id], (error, results) => {
     if (error) {
       res.status(500).json({ message: error.message });
